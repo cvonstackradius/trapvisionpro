55fc799a3d43e28ee60dea5df5d6944eb2f4d8ac
Christian von Stackelberg <christian@radiuswealth.com>
2026-09-18 20:28:57 -0400

    Initial Commit

---
+++ TrapVisionPro/TrapVisionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrapVisionProApp.swift
+//  TrapVisionPro
+//
+//  Created by christian von Stackelberg on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct TrapVisionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
